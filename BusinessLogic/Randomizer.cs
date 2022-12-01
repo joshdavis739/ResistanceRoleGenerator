8d67efbba2137e3215c11cb4ff1e5a413cad2789
Zak Tonks <zak@Zaks-Air.lan>
2022-12-01 20:30:11 +0000

    Ensure crypto security

--- BusinessLogic/Randomizer.cs
+++ BusinessLogic/Randomizer.cs
@@ -1,13 +1,10 @@
-﻿namespace BusinessLogic
+﻿using System.Security.Cryptography;
+
+namespace BusinessLogic
 {
     public class Randomizer<T>
     {
-        public virtual IOrderedEnumerable<T> Randomize(ICollection<T> collection)
-        {
-            // TODO: ensure cryptographic security
-            var randomizer = new Random();
-
-            return collection.OrderBy(x => randomizer.Next());
-        }
+        public virtual IOrderedEnumerable<T> Randomize(ICollection<T> collection) =>
+            collection.OrderBy(x => RandomNumberGenerator.GetInt32(int.MaxValue));
     }
 }
